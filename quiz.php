b90f60c2cbb15d3c6979aa9bf88b7921851173d5
Pakato14 <danieloliveira14@gmail.com>
2019-05-10 19:00:50 -0300

    reorganizando o projeto

---
+++ quiz.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/estilo.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Arizonia" rel="stylesheet">
+  <title>Mais Estátistica</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center text-primary">Teste seu nível em Estátistica</h1>
+        <h2 class="text-center text-success">Bem vindo ao Teste!!!</h2>
+        <div class="card">
+            <h3 class="text-center card-header">Welcome</h3>
+        </div>
+    </div>
+    
+</body>
+</html>
